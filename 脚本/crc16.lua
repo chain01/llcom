8de056b7c9f2a972e83914c67935212a60338cbe
chain01 <15863338830@163.com>
2019-12-19 10:01:30 +0800

    Create crc16.lua
    crc16

---
+++ 脚本/crc16.lua
@@ -0,0 +1,26 @@
+return function(adu)
+	local crc
+
+	local function initCrc()
+		crc = 0xffff
+	end
+	local function updCrc(byte)
+		crc = crc ~ byte
+		for i = 1, 8 do
+			local j = crc & 1
+			crc = crc >> 1
+			if j ~= 0 then
+				crc = crc ~ 0xA001
+			end
+		end
+	end
+
+	local function getCrc(adu)
+		initCrc()
+		for i = 1, #adu do
+			updCrc(adu:byte(i))
+		end
+		return string.pack("I2", crc)
+	end
+	return getCrc(adu)
+end
